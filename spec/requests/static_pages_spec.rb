8adcc0f9bc83a1ce98ad66b0a72ad84182ddd118
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-06 22:27:32 +0300

    Add links checking to spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,4 +29,20 @@ describe "Static pages" do
 		it {should have_content('Contact Us')}
 		it {should have_title("#{page_title} | Contact")}		
 	end
+
+	it "should have the right links on the layout" do
+	    visit root_path
+	    click_link "About"
+	    expect(page).to have_title('About')
+	    click_link "Help"
+	    expect(page).to have_title('Help')
+	    click_link "Contact"
+	    expect(page).to have_title('Contact')
+	    click_link "Home"
+	    click_link "Sign up now!"
+	    expect(page).to have_title('Sign Up')
+	    click_link "sample app"
+	    expect(page).to have_title('Home')
+	end
+
 end
