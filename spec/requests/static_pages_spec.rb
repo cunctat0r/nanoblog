b628e8a60b24e67da8a9505d56063e9f721f095d
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-06 21:53:33 +0300

    Refactor spec for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,52 +4,29 @@ require 'rails_helper'
 
 describe "Static pages" do 
 	let(:page_title) {"Ruby on Rails Tutorial Sample App"}
+	subject {page}
 
-	describe "Home page" do 
-		it "should have the content 'Sample App'" do
-			visit home_path
-			expect(page).to have_content('Sample App')
-		end
-
-		it "should have the right title" do
-  			visit home_path
-  			expect(page).to have_title("#{page_title} | Home")
-		end
+	describe "Home page" do
+		before {visit home_path} 
+		it {should have_content('Sample App')}
+		it {should have_title("#{page_title} | Home")}		
 	end
 
 	describe "Help page" do 
-		it "should have the content 'Help'" do
-			visit help_path
-			expect(page).to have_content('Help')
-		end
-
-		it "should have the right title" do
-  			visit help_path
-  			expect(page).to have_title("#{page_title} | Help")
-		end
+		before {visit help_path}
+		it {should have_content('Help')}
+		it {should have_title("#{page_title} | Help")}		
 	end
 
 	describe "About page" do 
-		it "should have the content 'About Us'" do
-			visit about_path
-			expect(page).to have_content('About Us')
-		end
-
-		it "should have the right title" do
-  			visit about_path
-  			expect(page).to have_title("#{page_title} | About")
-		end
+		before {visit about_path}
+		it {should have_content('About Us')}
+		it {should have_title("#{page_title} | About")}		
 	end
 
 	describe "Contact page" do 
-		it "should have the content 'Contact Us'" do
-			visit contact_path
-			expect(page).to have_content('Contact Us')
-		end
-
-		it "should have the right title" do
-  			visit contact_path
-  			expect(page).to have_title("#{page_title} | Contact")
-		end
+		before {visit contact_path}
+		it {should have_content('Contact Us')}
+		it {should have_title("#{page_title} | Contact")}		
 	end
 end
