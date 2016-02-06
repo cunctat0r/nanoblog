49637bb04c42474c00e7a140e8be26c04a12fcb7
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-06 22:15:42 +0300

    Add User controller, sugnup page and spec file

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "User Pages" do
+  subject {page}
+  describe "signup page" do
+  	before {visit signup_path}
+  	it { should have_content('Sign Up') }
+    it { should have_title('Sign Up') }
+  end
+end
