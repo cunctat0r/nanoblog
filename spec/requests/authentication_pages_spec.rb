f8c06fa907404160d32ee8e41afdcae377e7991e
Slava Petrov <slavik--petrov@yandex.ru>
2016-02-09 16:33:02 +0300

    Update spec
     "after saving the user" doesn't work

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,12 +26,10 @@ describe "Authentication" do
       end
     end
 
-   
-
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email", with: user.email.upcase
+        fill_in "Email", with: user.email
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
@@ -40,6 +38,21 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user))}
       it { should have_link('Sign out', href: signout_path)}
       it { should_not have_link('Sign in', href: signin_path)}
+
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'linus@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
 
   end
